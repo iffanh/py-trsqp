412356fe644f45d4be2d1f0007b0c39b0ae365e3
Muhammad Iffan Hannanu <iffan.hannanu@gmail.com>
2023-05-13 03:25:26 +0200

    update setup

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    packages=setuptools.find_packages('src', exclude=['test', 'utils']),
+    packages=setuptools.find_packages('py_trsqp', exclude=['test'], include=['utils']),
     name='py_trsqp',
     version='0.0.1',
     author='Muhammad Iffan Hannanu',
