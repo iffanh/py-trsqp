2c5b507bfc94b521a5a69a0e836c5b094c6ca3af
Muhammad Iffan Hannanu <iffan.hannanu@gmail.com>
2023-05-23 15:30:29 +0200

    add an equality constraint test

--- tests/test_trsqp.py
+++ tests/test_trsqp.py
@@ -8,6 +8,9 @@ def simple(x):
 def rosen(x:np.ndarray) -> np.ndarray: # Rosenbrock function: OF
     return 100*(x[1]-x[0]**2)**2+((x[0]-1)**2)
 
+def eq1(x):
+    return x[0] - x[1] - 1
+
 CONSTANTS = {}
 CONSTANTS["L_threshold"] = 1.005
 CONSTANTS["eta_1"] = 0.0001
@@ -28,6 +31,22 @@ class TRSQPTest(unittest.TestCase):
         
         self.assertAlmostEqual(tr.iterates[-1]['y_curr'][0], 1.0, places=4)
         self.assertAlmostEqual(tr.iterates[-1]['y_curr'][1], 1.0, places=4)
+        
+    def test_trsqp_simple1(self):
+        tr = tq.TrustRegionSQPFilter(x0=np.array([-2.5,-1.0]).T, 
+                                    k=6,
+                                    cf=simple, 
+                                    eqcs=[eq1], 
+                                    ineqcs=[],
+                                    opts={'solver': 'ipopt'}, 
+                                    constants=CONSTANTS)
+        tr.optimize(max_iter=100)
+        
+        sol0 = 36/28 #analytical solution
+        sol1 = 8/28
+        
+        self.assertAlmostEqual(tr.iterates[-1]['y_curr'][0], sol0, places=4)
+        self.assertAlmostEqual(tr.iterates[-1]['y_curr'][1], sol1, places=4)
         
     def test_trsqp_rosen(self):
         tr = tq.TrustRegionSQPFilter(x0=np.array([-2.5,-1.0]).T, 
