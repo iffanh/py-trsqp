18792939572ebc520386c4b03e127051b49412f1
Muhammad Iffan Hannanu <iffan.hannanu@gmail.com>
2023-11-14 14:11:58 +0100

    new version

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     # packages=setuptools.find_packages('py_trsqp', 'py_trsqp.utils', exclude=['test']),
     packages=['py_trsqp', 'py_trsqp.utils'],
     name='py_trsqp',
-    version='0.0.5a4',
+    version='0.0.5a5',
     author='Muhammad Iffan Hannanu',
     author_email='iffan.hannanu@gmail.com',
     description='First Official Release',
